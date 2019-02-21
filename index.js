3c49dc3080864cf7aad51160e6dbff2d6064ef01
Ryan <ryan-price@uiowa.edu>
2019-02-20 20:59:03 -0600

    Added mySQLto project

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+var mySql = require("mysql");
+var connection = mySql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: ""
+});
+connection.connect();
+var express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
