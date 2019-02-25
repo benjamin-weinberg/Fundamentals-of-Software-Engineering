043bcd53076c525e9b35eddc1ae5e604f4dcac0c
benjamin-weinberg <BenWeinberg01@gmail.com>
2019-02-25 16:34:55 -0600

    add routes to display pages

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path')
+var app = express()
+var router = express.Router();
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/home.html'));
+})
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/signup.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/login.html'));
+})
+
+
+app.listen(3000);
