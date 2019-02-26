9ca7298eb76e9d3e2df883b683902886ae5b94fb
benjamin-weinberg <BenWeinberg01@gmail.com>
2019-02-25 18:02:16 -0600

    add static files

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path')
 var app = express()
-var router = express.Router();
+
+
+app.use(express.static('public'));
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/home.html'));
