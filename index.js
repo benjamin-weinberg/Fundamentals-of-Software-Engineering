7b3eced86256d5a3c28a178db74a7409987dd37e
Ryan <ryan-price@uiowa.edu>
2019-02-26 09:06:37 -0600

    Added a possible connection to a mySql database

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var path = require('path')
-var app = express()
+var app = express();
 
+var mySql = require('mysql');
+
+var connection = mySql.createConnection({
+  host: '104.198.21.61',
+  user: 'serviceAccount',
+  password: 'password'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.use(express.static('public'));
 
